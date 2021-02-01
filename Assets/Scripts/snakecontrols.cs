1be40b7d54e18f12cbcd59419975e5faa66c544b
gianbartlo <gianbartolo2001@gmail.com>
2021-02-01 12:22:24 +0100

    Player colliding with walls results in level restart

---
+++ Assets/Scripts/snakecontrols.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class snakecontrols : MonoBehaviour
+{
+    public Transform player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            player.transform.position -= new Vector3(1f,0);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            player.transform.position += new Vector3(1f, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            player.transform.position += new Vector3(0, 1f);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            player.transform.position -= new Vector3(0, 1f);
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col){
+        SceneManager.LoadScene(1);
+
+    }
+
+
+}
