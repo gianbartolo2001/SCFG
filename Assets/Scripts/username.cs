f3c7aba34d3ed471aca24867f2a76c2ec1541924
gianbartlo <gianbartolo2001@gmail.com>
2021-02-01 12:54:22 +0100

    Username storing is added and displayed

---
+++ Assets/Scripts/username.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class username : MonoBehaviour
+{
+    public string playername;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+
+    void Update(){
+        playername = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = playername;
+    }
+
+    public void CreateUser(){
+        PlayerPrefs.SetString("username",playername);
+        string path = Application.dataPath + "/users/" + playername +".txt";
+        if (!File.Exists(path)){
+            File.WriteAllText(path,"");
+        }
+
+
+    }
+}
---
+++ Assets/getuser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class getuser : MonoBehaviour
+{
+   public GameObject textDisplay;
+    
+    void Start()
+    {
+        string playername = PlayerPrefs.GetString("username");
+    
+        textDisplay.GetComponent<Text>().text = playername;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
